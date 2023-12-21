c522efe21cfb6775021550789afc1aa4163091a4
Lordkaito <cespedesisai88@gmail.com>
2023-12-21 17:24:24 +0100

    Add server

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(express.json());
+
+app.post(`/newsletter`, async (req, res) => {
+  const { email } = req.body;
+
+  const result = await prisma.newsletter.create({
+    data: {
+      email,
+    },
+  });
+  res.json(result);
+});
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.newsletter.findMany();
+  res.json(users);
+});
+
+const server = app.listen(3000)
